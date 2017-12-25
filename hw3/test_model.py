956b0df4e2d6f6a8d55105a50e233981bbc68155
framr <pavel.sht@yandex.ru>
2017-12-26 00:25:17 +0300

    unittest -> py.test

--- hw3/test_model.py
+++ hw3/test_model.py
@@ -1,423 +1,149 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import unittest
-from model_v1 import AutoStorage, Validated, CharField, MISSING_FIELD, ArgumentsField, EmailField, GenderField,\
+import pytest
+
+from model_v1 import AutoStorage, Validated, CharField, ArgumentsField, EmailField,\
         BirthDayField, GenderField, ClientIDsField, PhoneField, DateField, ValidatedRequest
 from api import OnlineScoreRequest, ClientsInterestsRequest
 
 
-class TestAutoStorage(unittest.TestCase):
-    def setUp(self):
-        class T(object):
-            auto = AutoStorage()
-        self.t = T()
-
-    def test_auto_storage(self):
-        self.t.auto = 1
-        self.assertEquals(self.t.auto, 1)
-        self.t.auto = None
-        self.assertIsNone(self.t.auto)
+def test_auto_storage():
+    class T(object):
+        auto = AutoStorage()
+    t = T()
+    t.auto = 1
+    assert t.auto == 1
+    t.auto = None
+    assert t.auto is None
 
 
-class TestValidatedField(unittest.TestCase):
-    def test_required_not_nullable(self): 
-        class T(object):
-            val = Validated(required=True, nullable=False)
-        self.t = T()
-        
-        self.t.val = 1
-        self.assertEquals(self.t.val, 1)
-        self.t.val = "abc"
-        self.assertEquals(self.t.val, "abc")
-        try:
-            self.t.val = None
-        except ValueError:
-            print "Ok, ValueError raised"
-        else:
-            self.assertTrue(False, "ValueError not raised")
-        try:
-            self.t.val = MISSING_FIELD
-        except ValueError:
-            print "Ok, ValueError raised"
-        else:
-            self.assertTrue(False, "ValueError not raised")
-        try:
-            self.t.val = u""
-        except ValueError:
-            print "Ok, ValueError raised"
-        else:
-            self.assertTrue(False, "ValueError not raised")
-
-    def test_required_nullable(self): 
-        class T(object):
-            val = Validated(required=True, nullable=True)
-        self.t = T()
-        
-        self.t.val = 1
-        self.assertEquals(self.t.val, 1)
-        self.t.val = "abc"
-        self.assertEquals(self.t.val, "abc")
-        self.t.val = None
-        self.assertIsNone(self.t.val)
-        try:
-            self.t.val = MISSING_FIELD
-        except ValueError:
-            print "Ok, ValueError raised"
-        else:
-            self.assertTrue(False, "ValueError not raised")
-
-    def test_not_required_nullable(self): 
-        class T(object):
-            val = Validated(required=False, nullable=True)
-        self.t = T()
-        
-        self.t.val = 1
-        self.assertEquals(self.t.val, 1)
-        self.t.val = "abc"
-        self.assertEquals(self.t.val, "abc")
-        self.t.val = None
-        self.assertIsNone(self.t.val)
-        self.t.val = MISSING_FIELD
-        self.assertIsNone(self.t.val)
-
-    def test_not_required_not_nullable(self): 
-        class T(object):
-            val = Validated(required=False, nullable=False)
-        self.t = T()
-        
-        self.t.val = 1
-        self.assertEquals(self.t.val, 1)
-        self.t.val = "abc"
-        self.assertEquals(self.t.val, "abc")
-        try:
-            self.t.val = None
-        except ValueError:
-            print "Ok, ValueError raised"
-        else:
-            self.assertTrue(False, "ValueError not raised")
-
-        self.t.val = MISSING_FIELD
-        self.assertIsNone(self.t.val)
+@pytest.fixture()
+def req():
+    class T(object):
+        char = CharField(required=True, nullable=True)
+        args = ArgumentsField(required=True, nullable=True)
+        email = EmailField(required=False, nullable=True)
+        bday = BirthDayField(required=False, nullable=True) 
+        date = DateField(required=False, nullable=True)
+        gender = GenderField(required=False, nullable=True)
+        ids = ClientIDsField(required=True, nullable=False)
+        phone = PhoneField(required=False, nullable=True)
+    return T()
 
 
-class TestCharField(unittest.TestCase):
-    def test_none(self):
-        class Test(object):
-            char = CharField(required=True, nullable=True)
-        self.t = Test()
-        self.t.char = None
-        self.assertIsNone(self.t.char)
-
-    def test_valid_str(self):
-        class Test(object):
-            char = CharField(required=True, nullable=True)
-        self.t = Test()
-        self.t.char = u"abc"
-        self.assertEquals(self.t.char, u"abc")
-        self.t.char = u""
-        self.assertEquals(self.t.char, "")
-
-    def test_integer_raises(self):
-        class Test(object):
-            char = CharField(required=True, nullable=True)
-        self.t = Test()
-        try:
-            self.t.char = 1
-        except ValueError:
-            print "ValueError raised"
-        else:
-            self.assertTrue(False, "ValueError ont raised")
+@pytest.mark.parametrize("value", [1, []])
+def test_char_field_error_raises(value, req):
+    with pytest.raises(ValueError):
+        req.char = value
 
 
-class TestArgumentsField(unittest.TestCase):
-    def test_valid_dict(self):
-        class Test(object):
-            args = ArgumentsField(required=True, nullable=True)
-        self.t = Test()
-        try:
-            self.t.args = {}
-        except ValueError:
-            self.assertTrue(False, "ValueError raised on valid dict")
-        try:
-            self.t.args = {"1": 1, "2": 2}
-        except ValueError:
-            self.assertTrue(False, "ValueError raised on valid dict")
-
-    def test_not_valid_raises(self):
-        class Test(object):
-            args = ArgumentsField(required=True, nullable=True)
-        self.t = Test()
-        try:
-            self.t.args = ""
-        except ValueError:
-            pass
-        else:
-            self.assertTrue(False, "ValueError not raised")
+@pytest.mark.parametrize("value", ["1", "abc", u"abc", ""])
+def test_char_field_valid(value, req):
+    req.char = value
+    assert req.char == value
 
 
-class TestEmailField(unittest.TestCase):
-    def test_valid_email(self):
-        class Test(object):
-            email = EmailField(required=False, nullable=True)
-        self.t = Test()
-        try:
-            self.t.email = u""
-            self.t.email = None
-        except ValueError:
-            self.assertTrue(False, "ValueError raised on valid empty email")
-        try:
-            self.t.email = u"xxx@mail.ru"
-            self.assertEquals(self.t.email, u"xxx@mail.ru")
-            self.t.email = u"yyy@otus-python.ru"
-            self.assertEquals(self.t.email, u"yyy@otus-python.ru")
-        except ValueError:
-            self.assertTrue(False, "ValueError raised on valid email")
-
-    def test_not_valid_email_raises(self):
-        class Test(object):
-            email = EmailField(required=False, nullable=True)
-        self.t = Test()
-        try:
-            self.t.email = u"xxxmail.ru"
-        except ValueError:
-            print "ValueError raised on invalid email"
-        else:
-            self.assertTrue(False, "ValueError raised on valid email")
-        try:
-            self.t.email = 1
-        except ValueError:
-            print "ValueError raised on invalid email"
-        else:
-            self.assertTrue(False, "ValueError raised on valid email")
+@pytest.mark.parametrize("value", ["1", -1, 0, [], ()])
+def test_arguments_field_error_raises(value, req):
+    with pytest.raises(ValueError):
+        req.args = value
 
 
-class TestPhoneField(unittest.TestCase):
-    def test_valid_phone(self):
-        class Test(object):
-            phone = PhoneField(required=False, nullable=True)
-        t = Test()
-        try:
-            t.phone = u"79163332222"
-            self.assertEquals(t.phone, u"79163332222")
-            t.phone = 79163332222
-            self.assertEquals(t.phone, 79163332222)
-        except ValueError:
-            self.assertTrue(False, "ValueError raised on valid phone")
-
-    def test_not_valid_phone_raises(self):
-        class Test(object):
-            phone = PhoneField(required=False, nullable=True)
-        t = Test()
-        try:
-            t.phone = u"7916333222222222"
-        except ValueError:
-            print "ValueError raised on invalid phone"
-        else:
-            self.assertTrue(False, "ValueError not raised on invalid phone")
-        try:
-            t.phone = 7916333222222222
-        except ValueError:
-            print "ValueError raised on invalid phone"
-        else:
-            self.assertTrue(False, "ValueError not raised on invalid phone")
-        try:
-            t.phone = 7.111111111
-        except ValueError:
-            print "ValueError raised on invalid phone"
-        else:
-            self.assertTrue(False, "ValueError not raised on invalid phone")
-        try:
-            t.phone = u"7916333222a"
-        except ValueError:
-            print "ValueError raised on invalid phone"
-        else:
-            self.assertTrue(False, "ValueError not raised on invalid phone")
+@pytest.mark.parametrize("value", [{}, {1:1, 2:2, 3:3}, {"a": "b"}])
+def test_arguments_field_valid(value, req):
+    req.args = value
+    assert req.args == value
 
 
-class TestBirthDayField(unittest.TestCase):
-    def setUp(self):
-        class Test(object):
-            bday = BirthDayField(required=False, nullable=True)
-        self.t = Test()
- 
-    def test_valid_bday(self):    
-        self.t.bday = u"27.10.2000"
-        self.assertEquals(self.t.bday, u"27.10.2000")
-        self.t.bday = u"01.11.1980"
-        self.assertEquals(self.t.bday, u"01.11.1980")
-
-    def test_invalid_bday(self):
-        try:
-            self.t.bday = 27102000
-        except ValueError:
-            print "Ok, ValueError raised"
-        else:
-            self.assertTrue(False, "ValueError not raised")
-        try:
-            self.t.bday = u"27102000"
-        except ValueError:
-            print "Ok, ValueError raised"
-        else:
-            self.assertTrue(False, "ValueError not raised")
-        try:
-            self.t.bday = u"27-10-2000"
-        except ValueError:
-            print "Ok, ValueError raised"
-        else:
-            self.assertTrue(False, "ValueError not raised")
-        try:
-            self.t.bday = u"00.10.2000"
-        except ValueError:
-            print "Ok, ValueError raised"
-        else:
-            self.assertTrue(False, "ValueError not raised")
-        try:
-            self.t.bday = u"01.13.2000"
-        except ValueError:
-            print "Ok, ValueError raised"
-        else:
-            self.assertTrue(False, "ValueError not raised")
-        try:
-            self.t.bday = u"01.00.2000"
-        except ValueError:
-            print "Ok, ValueError raised"
-        else:
-            self.assertTrue(False, "ValueError not raised") 
-        try:
-            self.t.bday = u"lizard2000"
-        except ValueError:
-            print "Ok, ValueError raised"
-        else:
-            self.assertTrue(False, "ValueError not raised")
-        try:
-            self.t.bday = u"01.01.1900"
-        except ValueError:
-            print "Ok, ValueError raised"
-        else:
-            self.assertTrue(False, "ValueError not raised")
+@pytest.mark.parametrize("value", ["1", "", "myemail", "$$$", u"1", -1, 0, [], {}])
+def test_email_field_error_raises(value, req):
+    with pytest.raises(ValueError):
+        req.email = value
 
 
-class TestDateField(unittest.TestCase):
-    def setUp(self):
-        class Test(object):
-            date = DateField(required=False, nullable=True)
-        self.t = Test()
- 
-    def test_valid_date(self):    
-        self.t.date = u"27.10.2000"
-        self.assertEquals(self.t.date, u"27.10.2000")
-        self.t.date = u"01.11.1980"
-        self.assertEquals(self.t.date, u"01.11.1980")
-
-    def test_invalid_date(self):
-        try:
-            self.t.date = 27102000
-        except ValueError:
-            print "Ok, ValueError raised"
-        else:
-            self.assertTrue(False, "ValueError not raised")
-        try:
-            self.t.date = u"27102000"
-        except ValueError:
-            print "Ok, ValueError raised"
-        else:
-            self.assertTrue(False, "ValueError not raised")
-        try:
-            self.t.date = u"27-10-2000"
-        except ValueError:
-            print "Ok, ValueError raised"
-        else:
-            self.assertTrue(False, "ValueError not raised")
-        try:
-            self.t.date = u"00.10.2000"
-        except ValueError:
-            print "Ok, ValueError raised"
-        else:
-            self.assertTrue(False, "ValueError not raised")
-        try:
-            self.t.date = u"lizard2000"
-        except ValueError:
-            print "Ok, ValueError raised"
-        else:
-            self.assertTrue(False, "ValueError not raised")
-
-class TestGenderField(unittest.TestCase):
-    def setUp(self):
-        class T(object):
-            gender = GenderField(required=False, nullable=True)
-        self.t = T()
-
-    def test_valid_gender(self):
-        self.t.gender = 0
-        self.assertEquals(self.t.gender, 0)
-        self.t.gender = 1
-        self.assertEquals(self.t.gender, 1)
-        self.t.gender = 2
-        self.assertEquals(self.t.gender, 2)
-    
-    def test_invalid_gender(self):
-        try:
-            self.t.gender = -1
-        except ValueError:
-            print "Ok, ValueError raised"
-        else:
-            self.assertTrue(False, "ValueError not raised")
-        try:
-            self.t.gender = 3
-        except ValueError:
-            print "Ok, ValueError raised"
-        else:
-            self.assertTrue(False, "ValueError not raised")
-        try:
-            self.t.gender = "1"
-        except ValueError:
-            print "Ok, ValueError raised"
-        else:
-            self.assertTrue(False, "ValueError not raised")
+@pytest.mark.parametrize("value", [u"@", u"xxx@yyy", u"1@", u"@1", "xxx@yyy"])
+def test_email_field_valid(value, req):
+    req.email = value
+    assert req.email == value
 
 
-class TestClientIDsField(unittest.TestCase):
-    def setUp(self):
-        class T(object):
-            ids = ClientIDsField(required=True, nullable=False)
-        self.t = T()
-
-    def test_valid_clientids(self):
-        self.t.ids = [0, 1, 2]
-        self.assertEquals(self.t.ids, [0, 1, 2])
-        self.t.ids = []
-        self.assertEquals(self.t.ids, [])
-
-    def test_invalid_clientids(self):
-        try:
-            self.t.ids = ["0", "1", "2"]
-        except ValueError:
-            print "Ok, ValueError raised"
-        else:
-            self.assertTrue(False, "ValueError not raised")
-        try:
-             self.t.ids = {1:1, 2:2}
-        except ValueError:
-            print "Ok, ValueError raised"
-        else:
-            self.assertTrue(False, "ValueError not raised")
+@pytest.mark.parametrize("value", ["1", "", "myemail", "$$$", u"1", -1, 0, [], {},
+    "7-916-333-33-33", 49163333333])
+def test_phone_field_error_raises(value, req):
+    with pytest.raises(ValueError):
+        req.phone = value
 
 
-class TestValidatedRequest(unittest.TestCase):
-    def setUp(self):
-        class T(ValidatedRequest):
-            first_name = CharField(required=False, nullable=True)
-            last_name = CharField(required=False, nullable=True)
-            email = EmailField(required=False, nullable=True)
-            phone = PhoneField(required=False, nullable=True)
-            birthday = BirthDayField(required=False, nullable=True)
-            gender = GenderField(required=False, nullable=True)
-        self.t = T()
+@pytest.mark.parametrize("value", [u"79163333333", "79163333333", 79163333333])
+def test_phone_field_valid(value, req):
+    req.phone = value
+    assert req.phone == value
 
-    def test_find_validated_attrs(self): 
-        self.assertEquals(sorted(self.t._find_validated_attrs()),
-                          sorted("first_name last_name email phone birthday gender".split()))
-        
 
-if __name__ == "__main__":
-    unittest.main()
+@pytest.mark.parametrize("value", [u"27102000", u"2000.10.27", 27102000, u"27.10.1900"])
+def test__bday_error_raises(value, req):
+    with pytest.raises(ValueError):
+        req.bday = value
+
+
+@pytest.mark.parametrize("value", [u"27.10.2000", u"01.11.1980"])
+def test__bday_valid(value, req):
+    req.bday = value
+    assert req.bday == value
+
+
+@pytest.mark.parametrize("value", [u"27102000", u"2000.10.27", 27102000])
+def test__date_error_raises(value, req):
+    with pytest.raises(ValueError):
+        req.date = value
+
+
+@pytest.mark.parametrize("value", [u"27.10.2000", u"01.11.1980", u"27.10.1900"])
+def test__date_valid(value, req):
+    req.date = value
+    assert req.date == value
+
+
+@pytest.mark.parametrize("value", [-1, 3, 0.1, "abc"])
+def test__gender_error_raises(value, req):
+    with pytest.raises(ValueError):
+        req.gender = value
+
+
+@pytest.mark.parametrize("value", [0, 1, 2])
+def test__gender_valid(value, req):
+    req.gender = value
+    assert req.gender == value
+
+
+@pytest.mark.parametrize("value", [[""]])
+def test__ids_error_raises(value, req):
+    with pytest.raises(ValueError):
+        req.ids = value
+
+
+@pytest.mark.parametrize("value", [[0, 1, 2], []])
+def test__ids_valid(value, req):
+    req.ids = value
+    assert req.ids == value
+
+
+
+@pytest.fixture()
+def validated_req():
+    class T(ValidatedRequest):
+        first_name = CharField(required=False, nullable=True)
+        last_name = CharField(required=False, nullable=True)
+        email = EmailField(required=False, nullable=True)
+        phone = PhoneField(required=False, nullable=True)
+        birthday = BirthDayField(required=False, nullable=True)
+        gender = GenderField(required=False, nullable=True)
+    return T()
+
+
+def test_validated_request():
+    pass
+
+       
+
+
